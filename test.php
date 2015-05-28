19f01956634ebbd0ce46144821e5f14226c70bf3
garfieldhamilton <garfieldhamilton@gmail.com>
2015-05-28 15:55:21 -0400

    file update

---
+++ test.php
@@ -0,0 +1,12 @@
+ <php
+ 
+ $test = array(
+        "a" => "apple",
+        "b" => "bear",
+        "c" => "cheese",
+        "d" => "dog");
+foreach($test as $t)
+{
+  echo "item - ".$t."<BR>";
+}
+?>
